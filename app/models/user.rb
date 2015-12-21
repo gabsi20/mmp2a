80c1b8a60ee7ec36049704b4bbaef68c6357be7f
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2015-12-21 19:22:49 +0100

    add update user views
    updated user views for new columns
    updated user controller
    added create method for omniauth user creation in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
 	has_and_belongs_to_many :calendars
+
+	def self.create_with_omniauth(auth)
+	  create! do |user|
+	    user.provider = auth['provider']
+	    user.uid = auth['uid']
+	    if auth['info']
+	      user.firstname = auth['info']['first_name'] || ""
+	      user.lastname = auth['info']['last_name'] || ""
+	      user.email = auth['info']['email'] || ""
+	    end
+	  end
+	end
+
 end
