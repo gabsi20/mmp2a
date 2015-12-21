5b28c155a59d4cf4f82e8775e84527c92321c5c3
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2015-12-21 19:18:36 +0100

    add routes
    added routes for login/logout/

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,25 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+
   resources :calendars
   resources :users
+
+  get  '/login'  => 'sessions#new'
+  post '/login'  => 'sessions#create'
+  get  '/logout' => 'sessions#destroy'
+  get  '/' => 'welcome#index'
+
+  # omniauth route
+  get '/auth/:provider/callback', to: 'sessions#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
