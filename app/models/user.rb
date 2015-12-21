4fd4a45743f42e41b91f031dc2f151e73c08e2bd
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2015-12-21 21:28:13 +0100

    add create user with twitter
    updated create_with_omniauth
    added functions to split name into first and last name (twitter doesn't provide them seperately)
    input for e-mail has to be implemented (twitter doesn't provide e-mail addresses)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,33 @@
 class User < ActiveRecord::Base
 	has_and_belongs_to_many :calendars
 
-	def self.create_with_omniauth(auth)
-	  create! do |user|
+	def self.create_with_omniauth auth
+		create! do |user|
 	    user.provider = auth['provider']
 	    user.uid = auth['uid']
 	    if auth['info']
-	      user.firstname = auth['info']['first_name'] || ""
-	      user.lastname = auth['info']['last_name'] || ""
-	      user.email = auth['info']['email'] || ""
+	    	if auth['provider'] == "twitter"
+	    		user.firstname = self.getfirstname auth || ""
+	    		user.lastname = self.getlastname auth || ""
+	    	else
+		      user.firstname = auth['info']['first_name'] || ""
+		      user.lastname = auth['info']['last_name'] || ""
+		      user.email = auth['info']['email'] || ""
+		    end
 	    end
 	  end
 	end
 
+	private
+		def self.getfirstname auth
+			if auth['info']['name'].split.count > 1
+	      auth['info']['name'].split[0..-2].join(' ')
+	    else
+	      auth['info']['name']
+	    end
+	  end
+
+	  def self.getlastname auth
+	  	auth['info']['name'].split.last
+	  end
 end
