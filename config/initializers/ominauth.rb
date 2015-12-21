6a81db881df76e22a583b93786c28738a6bcede2
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2015-12-21 21:30:00 +0100

    add twitterconfig
    added keys variable to omniauth initializer

--- config/initializers/ominauth.rb
+++ config/initializers/ominauth.rb
@@ -1,4 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
            :scope => 'email', :info_fields => 'email,first_name,last_name'
+
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
