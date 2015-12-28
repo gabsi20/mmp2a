e496c00a10b629963afca7ccfaaa51f389e230be
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2015-12-28 16:51:04 +0100

    change brackets
    because fu brackets

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,9 +5,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      set_flash_message :notice, :success, :kind => "Facebook" if is_navigational_format?
     else
-      set_flash_message(:notice, :failure, :kind => "Facebook", :reason => "something went wrong") if is_navigational_format?
+      set_flash_message :notice, :failure, :kind => "Facebook", :reason => "something went wrong" if is_navigational_format?
       redirect_to root_path
     end
   end
@@ -17,9 +17,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+      set_flash_message :notice, :success, :kind => "Google" if is_navigational_format?
     else
-      set_flash_message(:notice, :failure, :kind => "Google", :reason => "something went wrong") if is_navigational_format?
+      set_flash_message :notice, :failure, :kind => "Google", :reason => "something went wrong" if is_navigational_format?
       redirect_to root_path
     end
   end
