411f4546e1608657d216275974a4edf6dccd9d0a
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2015-12-28 17:59:10 +0100

    fix sign_in_and_redirect
    for some reason this helper method didn't work
    changed it to two seperate functions: sign_in redirect_to

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      sign_in @user, :event => :authentication #this will throw if @user is not activated
+      redirect_to @user
       set_flash_message :notice, :success, :kind => "Facebook" if is_navigational_format?
     else
       set_flash_message :notice, :failure, :kind => "Facebook", :reason => "something went wrong" if is_navigational_format?
@@ -16,7 +17,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      sign_in @user, :event => :authentication #this will throw if @user is not activated
+      redirect_to @user
       set_flash_message :notice, :success, :kind => "Google" if is_navigational_format?
     else
       set_flash_message :notice, :failure, :kind => "Google", :reason => "something went wrong" if is_navigational_format?
