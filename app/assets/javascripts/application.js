d2b8fd4913101453c8c87b992218f9e11a83a857
Alexander Gabriel <agabriel.mmt-b2014@fh-salzburg.ac.at>
2015-12-30 18:29:18 +0100

    add layouts for different resolutions
    - desktop
    - tablet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,9 +19,17 @@ $(document).ready(function(){
 	$('#burger').click(function(){
 		if($('.main_menu').css("display") == "none"){
 			$('.main_menu').css("display","block");
+			if(($(window).width() > 768)&&($(window).width() < 992)){
+				$('#content').removeClass("col-sm-12");
+				$('#content').addClass("col-sm-8");
+			}
 		}
 		else{
 			$('.main_menu').css("display","none");
+			if(($(window).width() > 768)&&($(window).width() < 992)){
+				$('#content').removeClass("col-sm-8");
+				$('#content').addClass("col-sm-12");
+			}
 		}
 	});
 });
