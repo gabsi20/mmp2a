c313ccc6245ce6b5db236e976e75630cd6495d52
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2016-01-02 16:28:56 +0100

    add update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230171112) do
+ActiveRecord::Schema.define(version: 20160102145443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20151230171112) do
     t.datetime "due"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "title"
   end
 
   create_table "tokens", force: :cascade do |t|
