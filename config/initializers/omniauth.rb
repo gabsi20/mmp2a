29a8366c062ae35dba5f5ffc81ca5e49fcca053e
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2016-01-02 18:18:52 +0100

    fix wrong uri

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,1 +1,1 @@
-OmniAuth.config.full_host = Rails.env.production? ? 'https://mmp2-gabriel-huber.herokuapp.com/' : 'http://localhost:3000'
+OmniAuth.config.full_host = Rails.env.production? ? 'https://mmp2-gabriel-huber.herokuapp.com' : 'http://localhost:3000'
