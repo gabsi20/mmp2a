8ca02e854d1edf2f88acbc82411ea1cc92e8ca0f
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2016-01-02 16:30:52 +0100

    add method for synching tasks

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -3,27 +3,43 @@ class SyncController < ApplicationController
 	require 'google/api_client'
 
 	def calendars
-		@result = @client.execute( 
+		calendar_result = @client.execute( 
 				:api_method => @service.calendar_list.get,
 				:parameters => {
 					'calendarId' => ''
 				}
     )
     
-    @result.data.items.each { |calendar|
+    calendar_result.data.items.each { |calendar|
     	if(Calendar.where("uid" => calendar["id"]).empty?)
-	    	Calendar.create(calendar)
+	    	Calendar.create calendar
+	    	tasks calendar["id"]
+	    	puts "don't\n"
   	  end
     }
 	end
 
-	def tasks
-		@result = @client.execute( 
-				:api_method => @service.calendar_list.get,
+	def tasks uid
+		puts "do\n"
+		task_result = @client.execute( 
+				:api_method => @service.events.list,
 				:parameters => {
-					'calendarId' => ''
+					'calendarId' => uid
 				}
     )
+
+		while true
+		  events = task_result.data.items
+		  events.each do |e|
+		    Task.create e
+		  end
+		  if !(page_token = task_result.data.next_page_token)
+		    break
+		  end
+		  task_result = client.execute(:api_method => service.events.list,
+		                          :parameters => {'calendarId' => 'primary',
+		                                          'pageToken' => page_token})
+		end
 	end
 
 	def setup
