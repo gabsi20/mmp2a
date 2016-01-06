6fc0c25fbb965c346a5ed687de7564e7a036d991
Alexander Gabriel <agabriel.mmt-b2014@fh-salzburg.ac.at>
2016-01-06 10:01:33 +0100

    add redirect to tasks if user logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  def index 
-  	render template: "pages/index.html"
+  def index
+  	if current_user
+  		redirect_to tasks_path
+  	else
+  		render template: "pages/index.html"
+  	end
   end
   def show 
   	render template: "pages/#{params[:page]}"
