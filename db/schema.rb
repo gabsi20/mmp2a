f4c2870edbc5420866413a23d257ebc209c97396
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2016-01-27 11:32:16 +0100

    add unique constraint calendars_users
    calendar user pairs can only be saved once

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106154516) do
+ActiveRecord::Schema.define(version: 20160127102401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160106154516) do
     t.datetime "updated_at",  null: false
   end
 
+  add_index "calendars_users", ["calendar_id", "user_id"], name: "index_calendars_users_on_calendar_id_and_user_id", unique: true, using: :btree
   add_index "calendars_users", ["calendar_id"], name: "index_calendars_users_on_calendar_id", using: :btree
   add_index "calendars_users", ["user_id"], name: "index_calendars_users_on_user_id", using: :btree
 
