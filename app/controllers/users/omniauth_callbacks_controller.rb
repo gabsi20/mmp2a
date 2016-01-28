37e91b45bb42f87764490b5830d9c5c580891493
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2016-01-28 15:19:51 +0100

    change facebook
    remove facebook auth handling

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,19 +1,4 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-
-  # def facebook
- #    @user = User.from_omniauth(request.env["omniauth.auth"])
- #    Token.from_omniauth request.env["omniauth.auth"], @user
-
- #    if @user.persisted?
- #      sign_in @user, :event => :authentication #this will throw if @user is not activated
- #      redirect_to '/tasks'
- #      set_flash_message :notice, :success, :kind => "Facebook" if is_navigational_format?
- #    else
- #      set_flash_message :notice, :failure, :kind => "Facebook", :reason => "something went wrong" if is_navigational_format?
- #      redirect_to root_path
- #    end
- #  end
-
   def google_oauth2
     @user = User.from_omniauth request.env["omniauth.auth"]
     Token.from_omniauth request.env["omniauth.auth"], @user
