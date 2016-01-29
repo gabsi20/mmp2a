0cbf55b5dafcd17f0aaafc80868904447ec4f14d
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2016-01-29 13:02:25 +0100

    fix present

--- app/models/task.rb
+++ app/models/task.rb
@@ -11,7 +11,7 @@ class Task < ActiveRecord::Base
       task.title = taskinfo.summary || ''
       task.description = taskinfo.description || ''
       if taskinfo.creator.present?
-        if taskinfo.creator.has_key? displayName
+        if taskinfo.creator.displayName.present?
           task.autor = taskinfo.creator.displayName || ''
         end
       end
