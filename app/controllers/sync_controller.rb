a13d875f2b058dd6c03d1f11c0d13b26d75b74b2
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2016-01-29 11:12:03 +0100

    change refactoring sync_controller
    change brackets to text - convention
    move api request in own function - remove duplicate code
    move array mapper in own function - readability
    move statement validation in own function - readability

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -19,7 +19,7 @@ class SyncController < ApplicationController
     Calendar.unsync params['selection'], current_user
 
     unless params['selection'].nil?
-      params['selection'].each{ |cid|
+      params['selection'].each do |cid|
         calendar_result = @client.execute(
             :api_method => @service.calendar_list.get,
             :parameters => {
@@ -36,49 +36,40 @@ class SyncController < ApplicationController
           tasks = Task.where(:calendar_id => calendar.id)
           Status.create_status_for_user tasks, current_user
         end
-      }
+      end
     end
 
     redirect_to tasks_path
   end
 
   def create_tasks uid, calendar
-    task_result = @client.execute(
-      :api_method => @service.events.list,
-      :parameters => {
-        'calendarId' => uid
-      }
-    )
+    task_result = get_events_from_google uid
+    events = task_result.data.items
 
-   events = task_result.data.items
     events.each do |event|
       Task.save_event_in_database event, calendar
     end
   end
 
   def sync
-    current_user.calendars.each{ |calendar|
-      calendar_result = @client.execute(
-        :api_method => @service.events.list,
-        :parameters => {
-          'calendarId' => calendar.uid
-        }
-      )
+    current_user.calendars.each do |calendar|
+      calendar_result = get_events_from_google calendar.uid
       events = calendar_result.data.items
-      event_ids = events.map do |event|
-        event.id
-      end
-      Task.all.each{ |task|
-        unless event_ids.any?{ |id| task.uid == id}
+      event_ids = get_event_ids events
+      tasks = Task.where(:calendar_id => calendar)
+
+      Task.all.each do |task|
+        if task_was_removed_from_google event_ids, task
           Task.delete_task_and_statuses task
         end
-      }
-      events.each{ |event|
-        unless Task.exists? :uid => event.id
+      end
+
+      events.each do |event|
+        if event_is_not_in_database tasks, event.id
           Task.save_event_in_database event, calendar
         end
-      }
-    }
+      end
+    end
 
     redirect_to tasks_path
   end
@@ -88,5 +79,30 @@ class SyncController < ApplicationController
     token = Token.where(:user => current_user, :provider => 'google_oauth2').first
     @client.authorization.access_token = token.fresh_token
     @service = @client.discovered_api('calendar', 'v3')
+  end
+
+  def task_was_removed_from_google event_ids, task
+    !event_ids.any?{ |id| task.uid == id}
+  end
+
+  def get_events_from_google calendar_id
+    @client.execute(
+      :api_method => @service.events.list,
+      :parameters => {
+        'calendarId' => calendar_id
+      }
+    )
+  end
+
+  def event_is_not_in_database tasks, event_id
+    !tasks.any? do |task|
+      task.uid == event_id
+    end
+  end
+
+  def get_event_ids events
+    events.map do |event|
+      event.id
+    end
   end
 end
